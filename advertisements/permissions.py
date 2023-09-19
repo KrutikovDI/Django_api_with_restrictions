3e49d36c0c20f1bea1f9bc44b3c5b83fe6c8693b
Danil Krutikov <kdanil.i@yandex.ru>
2023-09-19 19:40:22 +0700

    Fifth commit

--- advertisements/permissions.py
+++ advertisements/permissions.py
@@ -1,7 +1,7 @@
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsAutor(BasePermission):
-    def has_object_permissions(self, request, view, obj):       # разграничиваем действия: для просмотра и создания объекта не меняем логику,
+    def has_object_permission(self, request, view, obj):       # разграничиваем действия: для просмотра и создания объекта не меняем логику,
                                                                 # но право вносить изменения PATCH... /ЗАПРОСЫ с идентификатором/ даем только автору
         if request.method in SAFE_METHODS:
             return True
